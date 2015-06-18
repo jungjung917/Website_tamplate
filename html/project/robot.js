bb1c10871edd3f59ac4d38b3c81497136d000ec6
Zhengshi Lucia <zhengshizhao917@gmail.com>
2015-06-18 01:04:40 -0400

    projectpage

---
+++ html/project/robot.js
@@ -0,0 +1,22 @@
+$(".flash").click(function() {
+        $(".eyes").toggleClass('laser');
+});
+function changecolor() {
+	var red = Math.floor(Math.random() * 255);
+        var green = Math.floor(Math.random() * 255);
+	var blue = Math.floor(Math.random() * 255);
+	var randomRGBA = 'rgba('+red+','+green+','+blue+',1)';
+	return randomRGBA;
+}
+
+$(".Bcolor").click(function() {
+        var bcolor = changecolor();
+        $(".robot").css("background",bcolor);
+	$(".robot").css("z-index",-1);
+});
+$(".Rcolor").click(function() {
+        var rcolor = changecolor();
+	var rbrain = '120px solidi '+ rcolor;
+	$(".torso").css("border-color",rcolor);
+	$(".brain").css("background",rcolor);
+});
